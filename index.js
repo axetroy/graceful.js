11115d16d569303f201bcd9155e712f3ded5257c
axetroy <troy450409405@gmail.com>
2017-12-20 16:08:22 +0800

    feat: add options silence to hide log message and add uncaughtException/unhandledRejection listener

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const kill = require('tree-kill');
 
-function graceful() {
-  graceful.start();
-  graceful.exist();
+function graceful(silence) {
+  graceful.start(silence);
+  graceful.exist(silence);
+  graceful.uncaughtException(silence);
+  graceful.unhandledRejection(silence);
 }
 
 function log(message) {
@@ -14,13 +16,13 @@ function signalHandler(signal) {
   process.exit(128);
 }
 
-graceful.start = function() {
-  log('start.');
+graceful.start = function(silence) {
+  !silence && log('start.');
 };
 
-graceful.exist = function() {
+graceful.exist = function(silence) {
   process.on('exit', function(existCode) {
-    log(`Exit with code ${existCode}.`);
+    !silence && log(`Exit with code ${existCode}.`);
     kill(process.pid);
   });
 
@@ -31,6 +33,18 @@ graceful.exist = function() {
   process.on('SIGBREAK', signalHandler);
   // when close the terminal
   process.on('SIGHUP', signalHandler);
+};
+
+graceful.uncaughtException = function(silence) {
+  process.on('uncaughtException', function(err) {
+    !silence && console.error('Error caught in uncaughtException event:', err);
+  });
+};
+
+graceful.unhandledRejection = function(silence) {
+  process.on('unhandledRejection', function(reason, p) {
+    !silence && console.error('Unhandled Rejection at:', p, 'reason:', reason);
+  });
 };
 
 module.exports = graceful;
